f8b91291e24616527800b534d782b0ecbdf5d1b8
Gary King <gwking@metabang.com>
2006-02-02 13:49:38 -0500

    Added commands
    darcs-hash:20060202184938-3cc5d-00c8fbf3e2b917a4c987f3a697956fffe28581b0.gz

---
+++ dev/commands.lisp
@@ -0,0 +1,52 @@
+(in-package rw)
+
+(defun upload-website (system)
+  (let* ((def (find-system system))
+         (dest (if (sub-folder def)
+                 (format nil "gking@common-lisp.net:/project/~(~A~)/public_html/documentation" 
+                         (name def))
+                 (format nil "gking@common-lisp.net:/project/~(~A~)/public_html/~(~A~)/documentation" 
+                         (sub-folder def) (name def))))
+         (src (translate-logical-pathname
+               (make-pathname 
+                :host "user-home"
+                :name (string-downcase (name def))
+                :directory '(:absolute "darcs" "website" "output")))))
+    (format nil "~@{~A ~}" "rcopy" "--exclude" "*.tem" src dest)
+    #+Ignore
+    (shell-command "rcopy" "--exclude" "*.tem" src dest)))
+
+;;; ---------------------------------------------------------------------------
+
+(defun upload-documentation (system)
+  (let* ((def (find-system system))
+         (dest (if (sub-folder def)
+                 (format nil "gking@common-lisp.net:/project/~(~A~)/public_html" 
+                         (name def))
+                 (format nil "gking@common-lisp.net:/project/~(~A~)/public_html/~(~A~)" 
+                         (sub-folder def) (name def))))
+         (src (translate-logical-pathname
+               (make-pathname 
+                :host "user-home"
+                :name (string-downcase (name def))
+                :directory '(:absolute "darcs" "website" "output")))))
+    (format nil "~@{~A ~}" "rcopy" "--exclude" "*.tem" src dest)
+    #+Ignore
+    (shell-command "rcopy" "--exclude" "*.tem" src dest)))
+
+;;; ---------------------------------------------------------------------------
+
+(defun make-asdf-system (system)
+  )
+
+;;; ---------------------------------------------------------------------------
+
+(defun upload-source (system)
+  )
+
+;;; ---------------------------------------------------------------------------
+
+(defun whatsnew (system)
+  )
+
+
